96dd9ea5f6b6d77d2791f5b3253022c3d116ec25
CarlosGomes07 <carlosgomes1999@hotmail.com>
2026-01-06 16:00:46 -0300

    feat: update project configuration and dependencies; rename project to 'axe-bank'

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/AxeBank/">
         <Routes>
           <Route path="/" element={<Navigate to="/landing" replace />} />
           <Route path="/landing" element={<Index />} />
